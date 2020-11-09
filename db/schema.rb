932307bd6fba5c96c5a80d7f1a5ae9aa49e885f5
horacinis <horacinis#gmail.com>
2020-11-09 14:57:27 +1100

    installed Devise, removed User model and created a new one through Devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_014510) do
+ActiveRecord::Schema.define(version: 2020_11_09_035029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 2020_11_09_014510) do
     t.boolean "has_rego"
     t.boolean "purchased"
     t.bigint "colour_id", null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["colour_id"], name: "index_cars_on_colour_id"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
@@ -38,14 +38,19 @@ ActiveRecord::Schema.define(version: 2020_11_09_014510) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "name"
     t.string "surname"
     t.string "phone"
     t.string "suburb"
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "cars", "colours"
