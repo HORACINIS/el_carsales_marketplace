034df75ee94b593ef787008b8598f7537155879c
horacinis <horacinis#gmail.com>
2020-11-09 22:49:08 +1100

    added users controller through devise and configured the routes file, to work with that users controller. added the extra attributes to the sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # get 'cars/index'
-  # get 'cars/show'
+
+devise_for :users, controllers: {
+  sessions: 'users/sessions', 
+  registrations: 'users/registrations' 
+} 
 
 
   get 'cars/index', to: 'cars#index'
