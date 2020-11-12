01ea2baba909917978ab8ab728b203984a403330
horacinis <horacinis@gmail.com>
2020-11-13 00:40:27 +1100

    .

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,6 @@
 class CarsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :create, :edit_car]
+  before_action :authenticate_user!, only: [:create, :edit_car, :car_params]
+  # before_action :car_params
 
   def index
     @cars = Car.all
@@ -12,11 +13,18 @@ class CarsController < ApplicationController
 
   def create
     @cars = Car.new
+    @current_user = current_user
+    # @cars_create = Car.create(car_params)
+    # redirect_to cars_create_path
   end
 
   def edit_car
     @cars = Car.find(params[:id])
   end
+
+  # def car_params
+  #   params.require(:car).permit(:make, :model, :year, :kilometres, :price, :colour_id, :has_rego, :purchased, :user_id)
+  # end
 
 
 end
