628381fcc320d0007ccd247f5d21bd90a4e71246
horacinis <horacinis#gmail.com>
2020-11-17 13:17:20 +1100

    now cars, when purchased, go to their new owner purchased list, did some styling to index and show pages

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,8 @@
 class PaymentsController < ApplicationController
 	def success
+		@car = Car.find(params["carId"].to_i)
+		@car.purchased = true
+		@car.user_id = current_user.id
+		@car.save!
 	end
 end
